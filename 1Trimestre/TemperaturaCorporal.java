4bfd1a1912f3ed69869e3f481c5483c6e328e6b2
Gloria Jimena <gloriajimenez010@icloud.com>
2024-10-03 19:22:48 +0200

    Temperaturas

---
+++ 1Trimestre/TemperaturaCorporal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TemperaturaCorporal {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double temperatura;
+        System.out.println("Introduzca la temperatura: ");
+        temperatura = teclado.nextDouble();
+
+        if ((temperatura < 34) || (temperatura > 41)) {
+            System.out.println("Vuelva a tomar la temperatura, posible error");
+
+        } else if ((temperatura > 34.1) && (temperatura < 37.1)) {
+            System.out.println("Temperatura correcta");
+
+        } else if ((temperatura > 37.2) && (temperatura < 38.3)) {
+            System.out.println("Acuda al médico, puede estar enfermo");
+        } else if ((temperatura > 38.4) && (temperatura < 40.9)) {
+            System.out.println("URGENTE: acuda al médico");
+        }
+    }
+}
