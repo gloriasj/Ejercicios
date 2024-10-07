9c684f17efa9606b87da2cea8a15620e09345f08
Gloria Jimena <gloriajimenez010@icloud.com>
2024-10-07 11:01:45 +0200

    solucion dia de la semana

--- 1Trimestre/DiaSemana.java
+++ 1Trimestre/DiaSemana.java
@@ -6,8 +6,22 @@ public class DiaSemana {
         int num;
         System.out.println("Introduce un numero del 1 al 7: ");
         num= teclado.nextInt();
-        if(num >1){
-            System.out.println("El dia de la semana es:" + );
+        if(num ==1){
+            System.out.println("LUNES");
+        }else if (num == 2){
+            System.out.println("MARTES");
+        }else if (num==3) {
+           System.out.println("MERCOLES"); 
+        }else if (num == 4) {
+            System.out.println("JUEVES");
+        }else if (num==5) {
+            System.out.println("VIERNES");
+        }else if (num == 6) {
+            System.out.println("SABADO");
+        }else if (num == 7){
+            System.out.println("DOMINGO");
+        }else{
+            System.out.println("No existe ningun dia de la semana");
         }
 
     
---
+++ 1Trimestre/Hora.java
@@ -0,0 +1,9 @@
+import java.net.Socket;
+import java.util.Scanner;
+public class Hora {
+    public static void main(String[] args) {
+        int hora;
+        System.out.println("Indique la hora: ");
+        
+    }
+}
