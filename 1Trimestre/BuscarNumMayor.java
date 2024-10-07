7ecf9863a0325472934edbd3f31afda4bc85e07a
Gloria Jimena <gloriajimenez010@icloud.com>
2024-10-07 09:24:37 +0200

    2 Solucion buscar numero mayor

--- 1Trimestre/BuscarNumMayor.java
+++ 1Trimestre/BuscarNumMayor.java
@@ -16,7 +16,16 @@ public class BuscarNumMayor {
         }else if (num2 > num1 && num2 > num3) {
             System.out.println("El numero mayor es:" + num2);
             
-        }else if (num3 > num1 && num3>num2) {
+        }else if (num1 == num2 && num2==num3 && num3>num1 && num3>num2) {
+            System.out.println("El numero mayor es: " + num3);
+        }
+        else if (num1>num2 && num1>num3 && num2==num3 && num3==num2){
+            System.out.println("El numero mayor es: " + num1);
+        }
+        else if (num1 == num2 && num1==num3 && num2>num1 && num2>num3) {
+            
+        }
+        else if (num3 > num1 && num3>num2) {
             System.out.println("El numero mayor es:" + num3);
             
         }else if (num1 == num2 && num2 ==num3) {
