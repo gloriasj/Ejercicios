ed949aa4e7f8acda898128945196fd3d221545a0
Gloria Jimena <gloriajimenez010@icloud.com>
2024-10-08 13:51:54 +0200

    cambios hora

--- 1Trimestre/Hora.java
+++ 1Trimestre/Hora.java
@@ -1,9 +1,20 @@
-import java.net.Socket;
+
 import java.util.Scanner;
 public class Hora {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
         int hora;
         System.out.println("Indique la hora: ");
-        
+        hora=teclado.nextInt();
+
+        if ((hora >= 6) && (hora<=12)) {
+            System.out.println("BUENOS DIAS");
+        }else if ((hora >= 13) && (hora<=20)){
+            System.out.println("BUENAS TARDES");
+        }else if ((hora >=21) && (hora <=5)) {
+            System.out.println("BUENAS NOCHES");
+        }else{
+            System.out.println("No se puede indicar nada");
+        }
     }
 }
