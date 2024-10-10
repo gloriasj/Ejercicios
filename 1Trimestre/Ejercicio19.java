07da7efa126b02d638da470a10ad47280ba4f7db
Gloria Jimena <gloriajimenez010@icloud.com>
2024-10-10 16:03:05 +0200

    Contar hacia atras estructura for

---
+++ 1Trimestre/Ejercicio19.java
@@ -0,0 +1,8 @@
+public class Ejercicio19 {
+    public static void main(String[] args) {
+        for (int i = 550; i >=340; i -=20){
+            System.out.println(i);
+        }
+        
+    }
+}
