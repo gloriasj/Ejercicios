e1aa437d2d1d1f53200f9f37b86d3c0cbd7bc405
Gloria Jimena <gloriajimenez010@icloud.com>
2024-10-10 10:44:01 +0200

    Bucles

---
+++ 1Trimestre/BuclesEj.java
@@ -0,0 +1,9 @@
+public class BuclesEj {
+    public static void main(String[] args) {
+        int valor =0;
+        while (valor <10){
+            valor ++;
+            System.out.println("Valor: " + valor);
+        }
+    }
+}
