e5e3fe2b84e516f60eb9d5cb5161d11d6fabaa3e
Gloria Jimena <gloriajimenez010@icloud.com>
2024-10-10 16:10:13 +0200

    numeros hacia atras estructura while

---
+++ 1Trimestre/Ejercicio20.java
@@ -0,0 +1,11 @@
+public class Ejercicio20 {
+    public static void main(String[] args) {
+        int valor = 550;
+
+        while (valor >= 340) {
+            System.out.println(valor);
+            valor -=20;
+            
+        }
+    }
+}
