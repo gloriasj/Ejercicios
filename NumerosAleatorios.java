576baf4642927717a8100cc099ced727557c481a
Gloria Jimena <gloriajimenez010@icloud.com>
2024-10-10 11:09:59 +0200

    Numero Aleatorio

---
+++ NumerosAleatorios.java
@@ -0,0 +1,13 @@
+package Ejercicios;
+
+public class NumerosAleatorios {
+    public static void main(String[] args) {
+        int aleatorio = (int) (0 + Math.random() * (30-0 +1));
+        while (aleatorio<25) {
+            System.out.println("Numero Aleatorio: " + aleatorio);
+            aleatorio = (int) (0 + Math.random() * (30-0 +1));
+        }
+
+    }
+    
+}
