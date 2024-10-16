34d78ddb518a7a0cc9452900b8c3df5bc4c377a7
Gloria Jimena <gloriajimenez010@icloud.com>
2024-10-16 08:42:00 +0200

    terminar ejercicio

---
+++ 1Trimestre/Ejercicio24.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Ejercicio24 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int num1,num2,num3;
+        System.out.println("Introduce los numeros");
+    }
+}
