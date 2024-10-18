1e6e50678b6e2c8c3a8637099afc110d8da321e5
Gloria Jimena <gloriajimenez010@icloud.com>
2024-10-18 12:50:01 +0200

    numeros pares estructura continue

---
+++ 1Trimestre/Ejercicio36.java
@@ -0,0 +1,13 @@
+public class Ejercicio36 {
+    public static void main(String[] args) {
+        for (int i = 0; i <=10; i++) {
+            if (i %2== 0) {
+                if (i ==5) {
+                    continue;
+                }
+                System.out.println(i);
+            }
+            
+        }
+    }
+}
